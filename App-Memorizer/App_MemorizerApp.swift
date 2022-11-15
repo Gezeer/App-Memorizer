01bce2ea380684d37c1b0f8886f886404d8610f7
Geiziane Alexandre <geizealexandre@gmail.com>
2022-11-15 11:27:19 -0300

    Initial Commit

---
+++ App-Memorizer/App_MemorizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_MemorizerApp.swift
+//  App-Memorizer
+//
+//  Created by Geiziane alexandre on 15/11/22.
+//
+
+import SwiftUI
+
+@main
+struct App_MemorizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
